69aa701a7d87bcc4ab48ab996c35abd10e3cbbb5
HanselRm <20220897@itla.edu.do>
2024-04-01 17:29:32 -0400

    fix spaces

--- RealStateAppProg3.Core.Application/ViewModels/Users/SaveUserViewModel.cs
+++ RealStateAppProg3.Core.Application/ViewModels/Users/SaveUserViewModel.cs
@@ -8,27 +8,41 @@ namespace RealStateAppProg3.Core.Application.ViewModels.Users
     public class SaveUserViewModel
     {
         public string Id { get; set; }
+
         [Required(ErrorMessage = "Debes ingresar tu Nombre")]
         public string Name { get; set; }
+
         [Required(ErrorMessage = "Debes Ingresar tu Apellido")]
         public string LastName { get; set; }
+
         [Required(ErrorMessage = "Debes Ingresar tu Cedula")]
         public string Identification { get; set; }
+
         public bool IsActive { get; set; } = false;
-        [Required(ErrorMessage = "Debes Ingresar tu Correo")]
-        public string Email { get; set; }
-        [Required(ErrorMessage = "Debes Ingresar un Usuario")]
-        public string Username { get; set; }
+
         [Required(ErrorMessage = "Debes Ingresar el numero de telefono")]
         public string PhoneNumber { get; set; }
+
+        public string? PhotoProfileUrl { get; set; }
+
+        [Required(ErrorMessage = "Debes Ingresar la foto del user")]
+        public IFormFile file { get; set; }
+
+        [Required(ErrorMessage = "Debes Ingresar un Usuario")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Debes Ingresar tu Correo")]
+        public string Email { get; set; }
+
         [Required(ErrorMessage = "Debe colocar una contraseña")]
         public string Password { get; set; }
+
         [Compare(nameof(Password), ErrorMessage = "Las contraseñas no coiciden")]
         [Required(ErrorMessage = "Debe colocar una contraseña")]
         public string ConfirmPassword { get; set; }
+
         public string TypeUser { get; set; }
-        public string PhotoProfileUrl { get; set; }
-        public IFormFile file { get; set; }
+        
         public bool IsConfirm { get; set; }
         public bool HasError { get; set; }
         public string Error { get; set; }
