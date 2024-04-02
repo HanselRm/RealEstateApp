68c3ee3015ab1df85dda7a5f32647c0e487aa933
HanselRm <20220897@itla.edu.do>
2024-04-01 20:48:04 -0400

    DTO email request

---
+++ RealStateAppProg3.Core.Application/Dtos/Email/EmailRequest.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace RealStateAppProg3.Core.Application.Dtos.Email
+{
+    public class EmailRequest
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string From { get; set; }
+    }
+}
