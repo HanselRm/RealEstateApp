6e4cc547d4de32bebb05075eb6c45cde6f00e4d1
HanselRm <20220897@itla.edu.do>
2024-04-04 19:52:03 -0400

    servicio de user (solo contructor)

---
+++ RealStateAppProg3.Core.Application/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using RealStateAppProg3.Core.Application.Interfaces.Service;
+
+
+namespace RealStateAppProg3.Core.Application.Services
+{
+    public class UserService
+    {
+        private readonly IAccountService _accountServices;
+        private readonly IMapper _mapper;
+
+        public UserService(IAccountService accountServices, IMapper mapper)
+        {
+            _accountServices = accountServices;
+            _mapper = mapper;
+        }
+
+        //metodo de login
+
+    }
+}
