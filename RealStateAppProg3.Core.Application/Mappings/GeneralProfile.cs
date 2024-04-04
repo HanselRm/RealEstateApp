1feeb20362d8179e3ba073f7ffd0b66e8a018cbb
HanselRm <20220897@itla.edu.do>
2024-04-04 19:55:06 -0400

    map de AuthenticationRequest to LoginViewModel

--- RealStateAppProg3.Core.Application/Mappings/GeneralProfile.cs
+++ RealStateAppProg3.Core.Application/Mappings/GeneralProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using RealStateAppProg3.Core.Application.Dtos.Account;
 using RealStateAppProg3.Core.Application.ViewModels.TypeProperty;
 using RealStateAppProg3.Core.Application.ViewModels.TypeSale;
+using RealStateAppProg3.Core.Application.ViewModels.Users;
 using RealStateAppProg3.Core.Domain.Entities;
 
 namespace RealStateAppProg3.Core.Application.Mappings
@@ -22,6 +24,13 @@ namespace RealStateAppProg3.Core.Application.Mappings
                 .ForMember(x => x.Properties, opt => opt.Ignore());
 
             CreateMap<TypeProperty, TypePropertyViewModel>();
+            #endregion
+
+            #region AuthenticationRequest
+            CreateMap<AuthenticationRequest, LoginViewModel>()
+               .ForMember(x => x.HasError, opt => opt.Ignore())
+               .ForMember(x => x.Error, opt => opt.Ignore())
+               .ReverseMap();
             #endregion
         }
     }
