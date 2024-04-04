878704859f8b654905ebe7691d36be9573e73c8c
HanselRm <20220897@itla.edu.do>
2024-04-04 19:53:02 -0400

    LoginViewModel

---
+++ RealStateAppProg3.Core.Application/ViewModels/Users/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace RealStateAppProg3.Core.Application.ViewModels.Users
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Debe colocar el correo o nombre de usuario")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar una contraseña")]
+        public string Password { get; set; }
+        public bool? HasError { get; set; }
+        public string? Error { get; set; }
+    }
+}
