333248465a9508e599dd47702fd5bc3dfb47e918
Ander <richardanderdev009@gmail.com>
2024-04-04 19:36:02 -0400

    Creacion de los viewModels

---
+++ RealStateAppProg3.Core.Application/ViewModels/Property/PropertyViewModel.cs
@@ -0,0 +1,29 @@
+﻿
+using RealStateAppProg3.Core.Application.ViewModels.TypeProperty;
+using RealStateAppProg3.Core.Application.ViewModels.TypeSale;
+using RealStateAppProg3.Core.Domain.Entities;
+
+namespace RealStateAppProg3.Core.Application.ViewModels.Property
+{
+    public class PropertyViewModel
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public decimal Value { get; set; }
+        public int NumberRooms { get; set; }
+        public int SizeInMeters { get; set; }
+        public int Bathrooms { get; set; }
+        public int IdTypeProperty { get; set; }
+        public int IdTypeSale { get; set; }
+        public string UrlImage1 { get; set; }
+        public string? UrlImage2 { get; set; }
+        public string? UrlImage3 { get; set; }
+        public string? UrlImage4 { get; set; }
+        //id usuario
+        public string IdUser { get; set; }
+        public TypePropertyViewModel? TypeProperty { get; set; }
+        public TypeSaleViewModel? TypeSale { get; set; }
+        public List<UpgradesProperty>? Upgrades { get; set; }
+        public List<PropertyFav>? propertyFavs { get; set; }
+    }
+}
---
+++ RealStateAppProg3.Core.Application/ViewModels/Upgrades/UpgradeViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+
+using RealStateAppProg3.Core.Domain.Entities;
+
+namespace RealStateAppProg3.Core.Application.ViewModels.Upgrades
+{
+    public class UpgradeViewModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public List<Property>? Properties {get; set;}
+    }
+}
