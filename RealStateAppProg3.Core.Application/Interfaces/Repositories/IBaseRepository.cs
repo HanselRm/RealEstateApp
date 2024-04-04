da6cf9925bf8a4a88f9566b42b94934b15653068
Ander <richardanderdev009@gmail.com>
2024-04-04 19:35:51 -0400

    Creacion de los repositorios con sus interfaces

---
+++ RealStateAppProg3.Core.Application/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿
+namespace RealStateAppProg3.Core.Application.Interfaces.Repositories
+{
+    public interface IBaseRepository<T>
+    {
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> GetAllWithInclude(List<string> properties);
+        Task<T> GetByIdAsync(int id);
+        Task<T> SaveAsync(T entity);
+        Task<T> UpdateAsync(T entity, int id);
+        Task RemoveAsync(T entity);
+        Task<int> CountAsync();
+    }
+}
---
+++ RealStateAppProg3.Infrastructure.Persistence/Repositories/TypeSaleRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using RealStateAppProg3.Core.Application.Interfaces.Repositories;
+using RealStateAppProg3.Core.Domain.Entities;
+using RealStateAppProg3.Infrastructure.Persistence.Context;
+
+namespace RealStateAppProg3.Infrastructure.Persistence.Repositories
+{
+    public class TypeSaleRepository : BaseRepository<TypeSale>, ITypeSaleRepository
+    {
+        private readonly RealStateAppContext _ctx;
+        public TypeSaleRepository(RealStateAppContext ctx) : base(ctx)
+        {
+            _ctx = ctx;
+        }
+    }
+}
