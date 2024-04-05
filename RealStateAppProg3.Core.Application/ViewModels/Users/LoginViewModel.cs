d2eb181c0f066b76577af2a0adbe637e9213cc72
HanselRm <20220897@itla.edu.do>
2024-04-04 21:47:21 -0400

    fix name

--- RealStateAppProg3.Core.Application/ViewModels/Users/LoginViewModel.cs
+++ RealStateAppProg3.Core.Application/ViewModels/Users/LoginViewModel.cs
@@ -7,7 +7,7 @@ namespace RealStateAppProg3.Core.Application.ViewModels.Users
     public class LoginViewModel
     {
         [Required(ErrorMessage = "Debe colocar el correo o nombre de usuario")]
-        public string Email { get; set; }
+        public string UserName { get; set; }
 
         [Required(ErrorMessage = "Debe colocar una contraseña")]
         public string Password { get; set; }
