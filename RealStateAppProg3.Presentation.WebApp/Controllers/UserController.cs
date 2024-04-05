03954d3788a423b25ece2511f7e7aa1f20201dbd
HanselRm <20220897@itla.edu.do>
2024-04-04 20:39:41 -0400

    action para la pag de login

---
+++ RealStateAppProg3.Presentation.WebApp/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealStateAppProg3.Core.Application.ViewModels.Users;
+
+namespace RealStateAppProg3.Presentation.WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        //pagina de login
+        public IActionResult Index()
+        {
+            return View(new LoginViewModel());
+        }
+    }
+}
