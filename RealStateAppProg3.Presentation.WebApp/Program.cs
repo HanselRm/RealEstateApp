f60232add6682e89d0f378f16ede1919e7876d59
HanselRm <20220897@itla.edu.do>
2024-04-04 20:47:13 -0400

    use sessions

--- RealStateAppProg3.Presentation.WebApp/Program.cs
+++ RealStateAppProg3.Presentation.WebApp/Program.cs
@@ -50,6 +50,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+//usa sesiones
+app.UseSession();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
