6b6a8034017867f4a0fd571550b8f38ab8136373
HanselRm <20220897@itla.edu.do>
2024-04-04 20:44:58 -0400

    User controller

--- RealStateAppProg3.Presentation.WebApp/Controllers/UserController.cs
+++ RealStateAppProg3.Presentation.WebApp/Controllers/UserController.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RealStateAppProg3.Core.Application.Interfaces.Service;
+using RealStateAppProg3.Core.Application.Services;
 using RealStateAppProg3.Core.Application.ViewModels.Users;
 
 namespace RealStateAppProg3.Presentation.WebApp.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         //pagina de login
         public IActionResult Index()
         {
             return View(new LoginViewModel());
         }
+
     }
 }
