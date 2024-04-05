8adb03a42106f7a0812c9e6c64f385f7d21d492a
HanselRm <20220897@itla.edu.do>
2024-04-04 20:21:47 -0400

    ResetPasswordViewModel

---
+++ RealStateAppProg3.Core.Application/ViewModels/Users/ResetPasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace RealStateAppProg3.Core.Application.ViewModels.Users
+{
+    public class ResetPasswordViewModel
+    {
+        [Required(ErrorMessage = "Debes de colocar el correo del usuario. ")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Debes de tener un token. ")]
+        public string Token { get; set; }
+
+        [Required(ErrorMessage = "Debes Ingresar una Contraseña")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "debes confirmar tu contraseña")]
+        [Compare(nameof(Password), ErrorMessage = "Ambas contraseña deben coincidir")]
+        public string ConfirmPassword { get; set; }
+        public bool HasError { get; set; }
+        public string Error { get; set; }
+    }
+}
