0f4e8b31b67778856c4a80d944fb3fc391b3a023
Ander <richardanderdev009@gmail.com>
2024-04-05 22:18:54 -0400

    Controlador upgrade con los metodos de guardado

---
+++ RealStateAppProg3.Presentation.WebApp/Controllers/UpgradeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealStateAppProg3.Core.Application.Interfaces.Service;
+using RealStateAppProg3.Core.Application.ViewModels.Upgrades;
+
+namespace RealStateAppProg3.Presentation.WebApp.Controllers
+{
+    public class UpgradeController : Controller
+    {
+        private readonly IUpgradeService _upgradeService;
+        public UpgradeController(IUpgradeService upgradeService)
+        {
+            _upgradeService = upgradeService;
+        }
+
+        public IActionResult Save()
+        {
+            return View("Save",new SaveUpgradesViewModel());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(SaveUpgradesViewModel vm)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("Save",vm);
+            }
+            await _upgradeService.SaveAsync(vm);
+            return RedirectToRoute(new{ controller = "Upgrade", action = "Save"});
+        }
+    }
+}
