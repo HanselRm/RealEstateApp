c4bfed2895e32d20830d6eeb76b6680e404aaac1
Ander <richardanderdev009@gmail.com>
2024-04-07 17:25:38 -0400

    CRUDS de tipos de propiedad,mejoras,ventas con sus vistas

--- RealStateAppProg3.Presentation.WebApp/Controllers/UpgradeController.cs
+++ RealStateAppProg3.Presentation.WebApp/Controllers/UpgradeController.cs
@@ -11,10 +11,24 @@ namespace RealStateAppProg3.Presentation.WebApp.Controllers
         {
             _upgradeService = upgradeService;
         }
-
+        public async Task<IActionResult> Index()
+        {
+            var vm = await _upgradeService.GetAllAsync();
+            return View(vm);
+        }
         public IActionResult Save()
         {
             return View("Save",new SaveUpgradesViewModel());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(SaveUpgradesViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Save", vm);
+            }
+            await _upgradeService.UpdateAsync(vm,vm.Id);
+            return RedirectToRoute(new { controller = "Upgrade", action = "Index" });
         }
         [HttpPost]
         public async Task<IActionResult> Save(SaveUpgradesViewModel vm)
@@ -24,7 +38,18 @@ namespace RealStateAppProg3.Presentation.WebApp.Controllers
                 return View("Save",vm);
             }
             await _upgradeService.SaveAsync(vm);
-            return RedirectToRoute(new{ controller = "Upgrade", action = "Save"});
+            return RedirectToRoute(new{ controller = "Upgrade", action = "Index"});
+        }
+
+        public IActionResult Remove(int Id)
+        {
+            ViewBag.Id = Id;    
+            return View();
+        }
+        public async Task<IActionResult> ConfirmRemove(int id)
+        {
+            await _upgradeService.RemoveAsync(id);
+            return RedirectToRoute(new { controller = "Upgrade", action = "Index" });
         }
     }
 }
