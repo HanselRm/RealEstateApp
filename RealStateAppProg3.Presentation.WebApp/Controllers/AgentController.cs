dd999eaeeeeef0ed5f8969a009ca0a1d68dd4aef
HanselRm <20220897@itla.edu.do>
2024-04-07 16:10:09 -0400

    Controlador de agente

---
+++ RealStateAppProg3.Presentation.WebApp/Controllers/AgentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealStateAppProg3.Core.Application.Interfaces.Service;
+
+namespace RealStateAppProg3.Presentation.WebApp.Controllers
+{
+    public class AgentController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public AgentController(IUserService userService, IHttpContextAccessor contextAccessor)
+        {
+            _userService = userService;
+            _contextAccessor = contextAccessor;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //mi perfil
+        public async Task<IActionResult> MyProfile(int Id)
+        {
+
+            return View();
+        }
+    }
+}
