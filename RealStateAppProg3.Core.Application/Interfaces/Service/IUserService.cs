3e57d150cbc128285c31f70d4e306d601376fb76
HanselRm <20220897@itla.edu.do>
2024-04-07 16:35:32 -0400

    metodo GetByIdWithoutRol en interface

--- RealStateAppProg3.Core.Application/Interfaces/Service/IUserService.cs
+++ RealStateAppProg3.Core.Application/Interfaces/Service/IUserService.cs
@@ -10,6 +10,7 @@ namespace RealStateAppProg3.Core.Application.Interfaces.Service
         Task<AuthenticationResponse> LoginAsync(LoginViewModel vm);
         Task<SaveUserViewModel> RegisterAsync(SaveUserViewModel vm, string origin);
         Task<SaveUserViewModel> UpdateAsync(SaveUserViewModel vm);
+        Task<SaveUserViewModel> GetByIdWithoutRol(string id);
         Task<ResetPasswordResponse> ResetPassWordAsync(ResetPasswordViewModel vm);
         Task SignOutAsync();
     }
