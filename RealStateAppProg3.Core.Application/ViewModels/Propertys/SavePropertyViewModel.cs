8b9d494419f886e41010b96f7c08ff0857b41d86
Ander <richardanderdev009@gmail.com>
2024-04-07 19:12:09 -0400

    Configuracion de la subida de imagenes

--- RealStateAppProg3.Core.Application/ViewModels/Propertys/SavePropertyViewModel.cs
+++ RealStateAppProg3.Core.Application/ViewModels/Propertys/SavePropertyViewModel.cs
@@ -1,24 +1,50 @@
 ﻿
 
+using Microsoft.AspNetCore.Http;
 using RealStateAppProg3.Core.Application.ViewModels.UpgradeProperty;
+using System.ComponentModel.DataAnnotations;
 
 namespace RealStateAppProg3.Core.Application.ViewModels.Propertys
 {
     public class SavePropertyViewModel
     {
         public string Code { get; set; }
+        [Required]
+        [DataType(DataType.Text,ErrorMessage = "Debe colocar una descripcion")]
         public string Description { get; set; }
+        [Range(0,double.MaxValue)]
+        [Required(ErrorMessage = "Debe colocar un precio de venta")]
         public decimal Value { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar el numero de habitaciones")]
+        [Range(0,int.MaxValue)]
         public int NumberRooms { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar el tamaño")]
+        [Range(0,double.MaxValue)]
         public int SizeInMeters { get; set; }
+        [Required(ErrorMessage = "Debe colocar el numero de baños")]
+        [Range(0, int.MaxValue)]
         public int Bathrooms { get; set; }
+
+        [Range(0, int.MaxValue)]
         public int IdTypeProperty { get; set; }
+        [Range(0, int.MaxValue)]
         public int IdTypeSale { get; set; }
-        public string UrlImage1 { get; set; }
+
+        public string? UrlImage1 { get; set; }
         public string? UrlImage2 { get; set; }
         public string? UrlImage3 { get; set; }
         public string? UrlImage4 { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar minimo una foto de la propiedad")]
+        public IFormFile Img1 {  get; set; }
+        public IFormFile? Img2 { get; set; }
+        public IFormFile? Img3 { get; set; }
+        public IFormFile? Img4 { get; set; }
+
         public List<UpgradePropertyViewModel>? Upgrades { get; set; }
+        public List<int> UpgradesId { get; set; }
         //id usuario
         public string IdUser { get; set; }
     }
--- RealStateAppProg3.Presentation.WebApp/Controllers/PropertyController.cs
+++ RealStateAppProg3.Presentation.WebApp/Controllers/PropertyController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Client;
+using RealStateAppProg3.Core.Application.Helpers;
 using RealStateAppProg3.Core.Application.Interfaces.Service;
 using RealStateAppProg3.Core.Application.Services;
 using RealStateAppProg3.Core.Application.ViewModels.Propertys;
@@ -25,7 +26,7 @@ namespace RealStateAppProg3.Presentation.WebApp.Controllers
         {
             return View();
         }
-        
+
         public async Task<IActionResult> Save()
         {
             //tipos de venta
@@ -38,6 +39,34 @@ namespace RealStateAppProg3.Presentation.WebApp.Controllers
             ViewBag.Upgrades = upgrades;
             ViewBag.TypeProperties = typeProperties;
             return View("Save", new SavePropertyViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(SavePropertyViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Save", vm);
+            }
+            vm = await _propertyService.SaveAsync(vm);
+            vm.UrlImage1 = UploadFiles.UploadFile(vm.Img1, "Property", vm.Code);
+            //en caso de que hayan pasado mas de una imagen
+            if(vm.Img2 != null)
+            {
+                vm.UrlImage2 = UploadFiles.UploadFile(vm.Img2, "Property", vm.Code);
+            }
+            if (vm.Img3 != null)
+            {
+                vm.UrlImage3 = UploadFiles.UploadFile(vm.Img3, "Property", vm.Code);
+            }
+            if (vm.Img4 != null)
+            {
+                vm.UrlImage4 = UploadFiles.UploadFile(vm.Img4, "Property", vm.Code);
+
+            }
+            //actualizamos la entidad con la ruta de las imagenes agregadas
+            await _propertyService.UpdateAsync(vm, int.Parse(vm.Code));
+            return RedirectToRoute(new { controller = "Property", action = "Index" });
         }
     }
 }
