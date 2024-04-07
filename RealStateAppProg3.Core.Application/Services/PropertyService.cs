0c379b757544669ecd85c1190866a39de45ea8e7
Ander <richardanderdev009@gmail.com>
2024-04-07 19:06:01 -0400

    Creacion del controlador propiedad y modificacion de dicho servicio

--- RealStateAppProg3.Core.Application/Services/PropertyService.cs
+++ RealStateAppProg3.Core.Application/Services/PropertyService.cs
@@ -1,29 +1,38 @@
 ﻿
 using AutoMapper;
 using InternetBanking.Core.Application.Services;
+using Microsoft.AspNetCore.Http;
+using RealStateAppProg3.Core.Application.Dtos.Account;
+using RealStateAppProg3.Core.Application.Helpers;
 using RealStateAppProg3.Core.Application.Interfaces.Repositories;
 using RealStateAppProg3.Core.Application.Interfaces.Service;
 using RealStateAppProg3.Core.Application.ViewModels.Propertys;
 using RealStateAppProg3.Core.Domain.Entities;
+using System.Xml.Serialization;
 
 namespace RealStateAppProg3.Core.Application.Services
 {
     public class PropertyService : BaseService<PropertyViewModel, SavePropertyViewModel, Property>, IPropertyService
     {
         private readonly IPropertyRepository _propertyRepository;
+
         private readonly IMapper _mapper;
         private readonly IUpgradesPropertyRepository _upgradePropertyRepository;
+        private readonly IHttpContextAccessor contextAccessor;
         public PropertyService(IPropertyRepository propertyRepository,
                                IMapper mapper,
-                               IUpgradesPropertyRepository upgradesPropertyRepository) : base(propertyRepository,mapper)
+                               IUpgradesPropertyRepository upgradePropertyRepository) : base(propertyRepository,mapper)
         {
             _propertyRepository =  propertyRepository;
+            _upgradePropertyRepository = upgradePropertyRepository;
             _mapper = mapper;
         }
 
         public override async Task<SavePropertyViewModel> SaveAsync(SavePropertyViewModel vm)
         {
             var property = _mapper.Map<Property>(vm);
+            vm.Code = CodeGenerator.Unique9DigitsGenerator().ToString();
+            vm.IdUser = contextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user").Id; 
             property = await _propertyRepository.SaveAsync(property);
             foreach(var item in vm.Upgrades)
             {
---
+++ RealStateAppProg3.Presentation.WebApp/Controllers/PropertyController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using RealStateAppProg3.Core.Application.Interfaces.Service;
+using RealStateAppProg3.Core.Application.Services;
+using RealStateAppProg3.Core.Application.ViewModels.Propertys;
+
+namespace RealStateAppProg3.Presentation.WebApp.Controllers
+{
+    public class PropertyController : Controller
+    {
+        private readonly IPropertyService _propertyService;
+        private readonly ITypeSaleService _typeSaleService;
+        private readonly ITypePropertyService _typePropertyService;
+        private readonly IUpgradeService _upgradeService;
+        public PropertyController(IPropertyService propertyService, ITypeSaleService typeSaleService,
+            ITypePropertyService typePropertyService, IUpgradeService upgradeService)
+        {
+            _typeSaleService = typeSaleService;
+            _typePropertyService = typePropertyService;
+            _propertyService = propertyService;
+            _upgradeService = upgradeService;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public async Task<IActionResult> Save()
+        {
+            //tipos de venta
+            var typeSales = await _typeSaleService.GetAllAsync();
+            ViewBag.TypeSales = typeSales;
+            //tipo de propiedades
+            var typeProperties = await _typePropertyService.GetAllAsync();
+            //mejoras
+            var upgrades = await _upgradeService.GetAllAsync();
+            ViewBag.Upgrades = upgrades;
+            ViewBag.TypeProperties = typeProperties;
+            return View("Save", new SavePropertyViewModel());
+        }
+    }
+}
