f130bbb8a9466db39c37123b6722efe638957aa9
HanselRm <20220897@itla.edu.do>
2024-04-08 20:14:05 -0400

    MyProfile

--- RealStateAppProg3.Presentation.WebApp/Controllers/AgentController.cs
+++ RealStateAppProg3.Presentation.WebApp/Controllers/AgentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RealStateAppProg3.Core.Application.Helpers;
 using RealStateAppProg3.Core.Application.Interfaces.Service;
 using RealStateAppProg3.Core.Application.ViewModels.Users;
 
@@ -23,6 +24,35 @@ namespace RealStateAppProg3.Presentation.WebApp.Controllers
         {
             SaveUserViewModel vm = await _userService.GetByIdWithoutRol(Id);
             return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MyProfile(SaveUserViewModel vm)
+        {
+            if (ModelState["Name"].Errors.Any() || ModelState["LastName"].Errors.Any() || ModelState["Identification"].Errors.Any()
+                || ModelState["PhoneNumber"].Errors.Any() || ModelState["file"].Errors.Any() || ModelState["PhoneNumber"].Errors.Any()
+                || ModelState["Username"].Errors.Any() || ModelState["Email"].Errors.Any() || ModelState["Password"].Errors.Any()
+                || ModelState["ConfirmPassword"].Errors.Any() || ModelState["TypeUser"].Errors.Any())
+            {
+                return View(vm);
+            }
+            var origin = Request.Headers["origin"];
+            vm.PhotoProfileUrl = UploadFiles.UploadFile(vm.file, "User", vm.Id);
+
+            SaveUserViewModel response = await _userService.RegisterAsync(vm, origin);
+
+            if (response != null && response.HasError != true)
+            {
+                return RedirectToRoute(new { controller = "User", action = "Index" });
+            }
+            else
+            {
+                vm.HasError = response.HasError;
+                vm.Error = response.Error;
+                return View(vm);
+
+            }
+
         }
     }
 }
