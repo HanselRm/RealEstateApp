66fcc335cef964d5fbf4c54af5feae06d19a3c2c
HanselRm <20220897@itla.edu.do>
2024-04-09 14:16:06 -0400

    add migration

--- RealStateAppProg3.Infrastructure.Persistence/Migrations/20240409181419_add_fecha_to_property.Designer.cs
+++ RealStateAppProg3.Infrastructure.Persistence/Migrations/20240409181419_add_fecha_to_property.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RealStateAppProg3.Infrastructure.Persistence.Context;
 
@@ -11,9 +12,11 @@ using RealStateAppProg3.Infrastructure.Persistence.Context;
 namespace RealStateAppProg3.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(RealStateAppContext))]
-    partial class RealStateAppContextModelSnapshot : ModelSnapshot
+    [Migration("20240409181419_add_fecha_to_property")]
+    partial class add_fecha_to_property
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RealStateAppProg3.Infrastructure.Persistence/Migrations/20240409181419_add_fecha_to_property.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealStateAppProg3.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_fecha_to_property : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Property",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Property");
+        }
+    }
+}
