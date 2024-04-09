921ab12e0c83e3bec6ab10b53a204c69e7faf3f1
HanselRm <20220897@itla.edu.do>
2024-04-08 21:40:26 -0400

    Agent add property

--- RealStateAppProg3.Presentation.WebApp/Controllers/AgentController.cs
+++ RealStateAppProg3.Presentation.WebApp/Controllers/AgentController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RealStateAppProg3.Core.Application.Helpers;
 using RealStateAppProg3.Core.Application.Interfaces.Service;
+using RealStateAppProg3.Core.Application.Services;
+using RealStateAppProg3.Core.Application.ViewModels.Propertys;
 using RealStateAppProg3.Core.Application.ViewModels.Users;
 
 namespace RealStateAppProg3.Presentation.WebApp.Controllers
@@ -9,11 +11,20 @@ namespace RealStateAppProg3.Presentation.WebApp.Controllers
     {
         private readonly IUserService _userService;
         private readonly IHttpContextAccessor _contextAccessor;
+        private readonly IPropertyService _propertyService;
+        private readonly ITypeSaleService _typeSaleService;
+        private readonly ITypePropertyService _typePropertyService;
+        private readonly IUpgradeService _upgradeService;
 
-        public AgentController(IUserService userService, IHttpContextAccessor contextAccessor)
+        public AgentController(IUserService userService, IHttpContextAccessor contextAccessor, IPropertyService propertyService, ITypeSaleService typeSaleService,
+            ITypePropertyService typePropertyService, IUpgradeService upgradeService)
         {
             _userService = userService;
             _contextAccessor = contextAccessor;
+            _typeSaleService = typeSaleService;
+            _typePropertyService = typePropertyService;
+            _propertyService = propertyService;
+            _upgradeService = upgradeService;
         }
         public IActionResult Index()
         {
@@ -49,7 +60,48 @@ namespace RealStateAppProg3.Presentation.WebApp.Controllers
                 return View(vm);
 
             }
+        }
 
+        public async Task<IActionResult> SaveProperty()
+        {
+            //tipos de venta
+            var typeSales = await _typeSaleService.GetAllAsync();
+            ViewBag.TypeSales = typeSales;
+            //tipo de propiedades
+            var typeProperties = await _typePropertyService.GetAllAsync();
+            //mejoras
+            var upgrades = await _upgradeService.GetAllAsync();
+            ViewBag.Upgrades = upgrades;
+            ViewBag.TypeProperties = typeProperties;
+            return View("SaveProperty", new SavePropertyViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveProperty(SavePropertyViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Save", vm);
+            }
+            vm = await _propertyService.SaveAsync(vm);
+            vm.UrlImage1 = UploadFiles.UploadFile(vm.Img1, "Property", vm.Code);
+            //en caso de que hayan pasado mas de una imagen
+            if (vm.Img2 != null)
+            {
+                vm.UrlImage2 = UploadFiles.UploadFile(vm.Img2, "Property", vm.Code);
+            }
+            if (vm.Img3 != null)
+            {
+                vm.UrlImage3 = UploadFiles.UploadFile(vm.Img3, "Property", vm.Code);
+            }
+            if (vm.Img4 != null)
+            {
+                vm.UrlImage4 = UploadFiles.UploadFile(vm.Img4, "Property", vm.Code);
+
+            }
+            //actualizamos la entidad con la ruta de las imagenes agregadas
+            await _propertyService.UpdateAsync(vm, int.Parse(vm.Code));
+            return RedirectToRoute(new { controller = "Property", action = "Index" });
         }
     }
 }
