d45374a1e8ac73028a33037adbf20734718abc60
HanselRm <20220897@itla.edu.do>
2024-04-09 15:58:52 -0400

    Fix en generar numero ramdon helper

--- RealStateAppProg3.Core.Application/Helpers/CodeGenerator.cs
+++ RealStateAppProg3.Core.Application/Helpers/CodeGenerator.cs
@@ -16,13 +16,16 @@ namespace RealStateAppProg3.Core.Application.Helpers
             Random random = new Random();
 
             // Generar un número aleatorio de 3 dígitos (entre 0 y 999)
-            int randomNumber = random.Next(1000);
+            int randomNumber = random.Next(100000, 1000000); // Generar un número de 6 dígitos (entre 100000 y 999999)
 
             // Combina el timestamp y el número aleatorio para formar el código único
-            long uniqueCode = (timestamp * 1000) + randomNumber;
+            long uniqueCode = (timestamp * 1000000) + randomNumber; // Multiplicar el timestamp por 1000000 para garantizar un código de 6 dígitos
 
-            //devolver el numero
-            return (int)uniqueCode;
+            // Asegura de que el número generado sea siempre positivo
+            int positiveUniqueCode = Math.Abs((int)uniqueCode);
+
+            // Devolver el número
+            return positiveUniqueCode;
         }
     }
 }
