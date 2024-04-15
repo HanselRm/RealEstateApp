dabd3c1d4770162d95548e61a7ac7ada2d0cc1eb
HanselRm <20220897@itla.edu.do>
2024-04-14 21:23:20 -0400

    Add id to PropertyFavViewModel

--- RealStateAppProg3.Core.Application/ViewModels/PropertyFav/PropertyFavViewModel.cs
+++ RealStateAppProg3.Core.Application/ViewModels/PropertyFav/PropertyFavViewModel.cs
@@ -5,6 +5,7 @@ namespace RealStateAppProg3.Core.Application.ViewModels.PropertyFav
 {
     public class PropertyFavViewModel
     {
+        public int Id { get; set; }
         public string IdUser { get; set; }
         public string IdProperty { get; set; }
 
