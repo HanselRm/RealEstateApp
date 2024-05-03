6603df6fbe09a49fd98e22c7181323d48a532a76
HanselRm <20220897@itla.edu.do>
2024-05-03 15:00:40 -0400

    Fix save admin and dev

--- RealStateAppProg3.Presentation.WebApp/Controllers/AdminController.cs
+++ RealStateAppProg3.Presentation.WebApp/Controllers/AdminController.cs
@@ -71,7 +71,7 @@ namespace RealStateAppProg3.Presentation.WebApp.Controllers
         public async Task<IActionResult> SaveAdminUser(SaveUserViewModel vm)
         {
             if (ModelState["Name"].Errors.Any() || ModelState["LastName"].Errors.Any() || ModelState["Identification"].Errors.Any()
-                || ModelState["PhoneNumber"].Errors.Any() || ModelState["file"].Errors.Any() || ModelState["PhoneNumber"].Errors.Any()
+                || ModelState["PhoneNumber"].Errors.Any() || ModelState["PhoneNumber"].Errors.Any()
                 || ModelState["Username"].Errors.Any() || ModelState["Email"].Errors.Any() || ModelState["Password"].Errors.Any()
                 || ModelState["ConfirmPassword"].Errors.Any())
             {
@@ -177,9 +177,9 @@ namespace RealStateAppProg3.Presentation.WebApp.Controllers
             }
             vm.PhotoProfileUrl = usr.PhotoProfileUrl;
             if (ModelState["Name"].Errors.Any() || ModelState["LastName"].Errors.Any() || ModelState["Identification"].Errors.Any()
-                  || ModelState["PhoneNumber"].Errors.Any() || ModelState["file"].Errors.Any() || ModelState["PhoneNumber"].Errors.Any()
-                  || ModelState["Username"].Errors.Any() || ModelState["Email"].Errors.Any() || ModelState["Password"].Errors.Any()
-                  || ModelState["ConfirmPassword"].Errors.Any())
+                || ModelState["PhoneNumber"].Errors.Any() || ModelState["PhoneNumber"].Errors.Any()
+                || ModelState["Username"].Errors.Any() || ModelState["Email"].Errors.Any() || ModelState["Password"].Errors.Any()
+                || ModelState["ConfirmPassword"].Errors.Any())
             {
                 return View("RegisterAdmin", vm);
             }
